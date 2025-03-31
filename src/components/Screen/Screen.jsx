a5c0ece4c708466d934566a97f4ad5ddc80d0098
Vayntail <113714157+vayntail@users.noreply.github.com>
2025-03-30 23:30:19 -0400

    Fixed resolutions in orientation context by adding portrait vs. landscape

--- src/components/Screen/Screen.jsx
+++ src/components/Screen/Screen.jsx
@@ -3,19 +3,28 @@ import { useScreenOrientation } from "../../contexts/ScreenOrientationContext";
 import TopBar from "../TopBar/TopBar";
 import NavButtons from "../NavButtons/NavButtons";
 import Windows from "../Windows/Windows";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useWindow } from "../../contexts/WindowContext";
 
 const Screen = () => {
-  const { isMobile, isHorizontal } = useScreenOrientation();
+  const { isMobile, isHorizontal, screenOrientationRef, isPortrait } = useScreenOrientation();
   const { screenRef } = useWindow();
+
+  // when either context refs change, set merged context and update both contexts.
+  const mergedScreenRef = useRef(null);
+  useEffect(() => {
+    if (mergedScreenRef.current) {
+      screenOrientationRef.current = mergedScreenRef.current;
+      screenRef.current = mergedScreenRef.current;
+    }
+  }, [screenOrientationRef, screenRef])
 
   return (
     <div
-      ref={screenRef}
+      ref={mergedScreenRef}
       className={`Screen ${isMobile ? "mobile" : "desktop"} ${
-        isHorizontal && "horizontal"
-      }`}
+        isHorizontal? "horizontal": "vertical"
+      } ${isPortrait ? "portrait" : "landscape"}`}
     >
       <TopBar />
       <NavButtons />
--- src/contexts/ScreenOrientationContext.jsx
+++ src/contexts/ScreenOrientationContext.jsx
@@ -1,18 +1,30 @@
-import { createContext, useState, useEffect, useContext } from "react";
+import { createContext, useState, useEffect, useContext, useRef } from "react";
 
 const ScreenOrientationContext = createContext();
 
 export const ScreenOrientationProvider = ({ children }) => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 480);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 480 || window.innerHeight < 270);
   const [isHorizontal, setIsHorizontal] = useState(
-    !isMobile && window.innerWidth > window.innerHeight
+    window.innerWidth > window.innerHeight
   );
+  // if window is re-sized in a way where black bars appear at top and bottom, it is a portrait.
+  // if it appears at left and right, it is not a portrait, but a landscape.
+  const [isPortrait, setIsPortrait] = useState(null); 
+  const screenOrientationRef = useRef(null);
 
   // handle re-sizing of window
   useEffect(() => {
+    setIsPortrait(window.innerHeight > screenOrientationRef.current.clientHeight);
+    console.log(window.innerHeight > screenOrientationRef.current.clientHeight)
+
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 480);
-      setIsHorizontal(!isMobile && window.innerWidth > window.innerHeight);
+      setIsMobile(window.innerWidth < 480 || window.innerHeight < 270);
+      setIsHorizontal(window.innerWidth > window.innerHeight);
+      setIsPortrait(window.innerHeight > screenOrientationRef.current.clientHeight);
+
+      // test logs
+      console.log(window.innerHeight > screenOrientationRef.current.clientHeight)
+      console.log(window.innerWidth < 480 || window.innerHeight < 270, window.innerWidth > window.innerHeight)
     };
 
     window.addEventListener("resize", handleResize);
@@ -20,7 +32,7 @@ export const ScreenOrientationProvider = ({ children }) => {
   }, []);
 
   return (
-    <ScreenOrientationContext.Provider value={{ isMobile, isHorizontal }}>
+    <ScreenOrientationContext.Provider value={{ isMobile, isPortrait, isHorizontal, screenOrientationRef }}>
       {children}
     </ScreenOrientationContext.Provider>
   );
